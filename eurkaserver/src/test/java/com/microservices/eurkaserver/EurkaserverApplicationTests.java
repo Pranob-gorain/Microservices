2f380a62ab43f5d6132fb16e23173944eb2dc32e
pranob gorain <pranob@liv-ex.com>
2023-04-25 23:22:47 +0100

    Added Eurek server

---
+++ eurkaserver/src/test/java/com/microservices/eurkaserver/EurkaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.eurkaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurkaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
